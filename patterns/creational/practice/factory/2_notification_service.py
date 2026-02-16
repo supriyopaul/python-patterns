6ca0a04e26cdc5f9582d594ae15d6c6eef82b538
Paul Supriyo (BT: Product Dev. A1029478 Eng.: CoE Engineering) <supriyo.paul@hm.com>
2026-02-16 16:18:57 +0530

    Update Notification Service implementation with consolidated interface

--- patterns/creational/practice/factory/2_notification_service.py
+++ patterns/creational/practice/factory/2_notification_service.py
@@ -25,3 +25,31 @@ Example Output:
 Sending SMS: Hello World
 Sending Email: Welcome aboard!
 """
+
+from typing import Protocol
+
+class Notifier(Protocol):
+    def send(self, message, to):
+        pass
+
+class EmailNotifier:
+    def send(self, message, to):
+        return "Sent an email notification to: {to}\n with body:{message}".format(to=to, message=message)
+
+class SMSNotifier:
+    def send(self, message, to):
+        return "Sent an SMS notification to: {to}\n message: {message}".format(to=to, message=message)
+
+def send_notification(notificationtype="Email") -> Notifier:
+    if notificationtype == "Email":
+        return EmailNotifier()
+    elif notificationtype == "SMS":
+        return SMSNotifier()
+    else:
+        raise ValueError("Wrong notification type")
+
+if __name__ == "__main__":
+    print(send_notification("SMS").send(message="Sample message", to=123456789))
+    print(send_notification("Email").send(message="body of the email", to="paul.supriyo@gmail.com"))
+    print(send_notification("Email").send(message="body of the email")) #wrong
+    print(send_notification("Slack").send())
