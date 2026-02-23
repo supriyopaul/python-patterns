f946b183c10a55f966770ec22fdd3f252e678b67
Paul Supriyo (BT: Product Dev. A1029478 Eng.: CoE Engineering) <supriyo.paul@hm.com>
2026-02-23 14:42:06 +0530

    Fix logic bugs and typos in Database Connector practice solution

--- patterns/creational/practice/factory/3_database_connector.py
+++ patterns/creational/practice/factory/3_database_connector.py
@@ -27,3 +27,76 @@ Example Output:
 Connecting to SQLite...
 SQLite executing: SELECT * FROM users
 """
+
+from typing import Protocol
+
+class DBConnector(Protocol):
+    
+    def connect(self):
+        pass
+    
+    def execute(self, connection, sql_string:str):
+        pass
+
+
+def parse_connection_url(url):
+    sql_type = url.split("://")[0]
+    host_port = url.split("://")[1]
+    host = host_port.split(':')[0]
+    port = host_port.split(':')[1]
+    return (sql_type.lower(), host.lower(), port.lower())
+
+class SQLiteConnector():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.is_connected = False
+    
+    def connect(self):
+        if self.host and self.port:
+            print("Connecting to SQLite at {host}:{port}".format(host=self.host, port=self.port))
+            self.is_connected = True
+    
+    def execute(self, sql_string:str):
+        if not self.is_connected:
+            self.connect()
+        print("SQLite executing sql: {sql_string}".format(sql_string=sql_string))
+
+
+class MySQLConnector():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.is_connected = False
+    
+    def connect(self):
+        if self.host and self.port:
+            print("Connecting to MySQL at {host}:{port}".format(host=self.host, port=self.port))
+            self.is_connected = True
+    
+    def execute(self, sql_string:str):
+        if not self.is_connected:
+            self.connect()
+        print("MySQL executing sql: {sql_string}".format(sql_string=sql_string))
+
+def get_db_connector(url:str):
+    connection_info = parse_connection_url(url)
+    db_type = connection_info[0]
+    host = connection_info[1]
+    port = connection_info[2]
+
+    if db_type == "sqlite":
+        return SQLiteConnector(host=host, port=port)
+    elif db_type == "mysql":
+        return MySQLConnector(host=host, port=port)
+    else:
+        raise ValueError("Invalid SQL type: {db_type}".format(db_type=db_type))
+
+if __name__ == '__main__':
+    url1 = 'sqlite://localhost:5132'
+    url2 = 'mysql://remote-server:3306'
+    sqlite_connection = get_db_connector(url1)
+    mysql_connection = get_db_connector(url2)
+
+    sqlite_connection.execute("Select * from users limit 10;")
+    mysql_connection.execute("Select * from customers limit 5;")
